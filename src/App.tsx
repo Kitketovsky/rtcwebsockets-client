1e9bac234b8b9efe155b8b663bd09491d8fd065e
Kitketovsky <kitketovsky@gmail.com>
2023-01-28 20:46:58 +0400

    Testing WebSocket on client side

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useRef } from "react";
+
 function App() {
-  return <div>Hello React!</div>;
+  const ws = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    if (ws.current) return;
+
+    ws.current = new WebSocket("wss://rtcwebsockets-server.herokuapp.com");
+
+    ws.current.addEventListener("open", () => {
+      console.log("CLIENT:: WebSocket connection is established...");
+    });
+
+    ws.current?.addEventListener("message", (event) => {
+      console.log(event.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <button onClick={() => ws.current?.send("Hello from client!")}>
+        Send Hello!
+      </button>
+    </div>
+  );
 }
 
 export default App;
